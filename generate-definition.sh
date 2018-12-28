d99e9d94d20dae17bcd2bfd7acc1d756f20752f0
Hamilton Lima <hamilton.lima@gmail.com>
2018-12-28 18:36:57 -0500

    update default game definition with gamecomponents

---
+++ generate-definition.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+PWD=$(pwd)
+docker run -it -v $PWD:/tmp robolucha-runner \
+/usr/bin/java -cp /usr/src/app/robolucha-runner.jar com.robolucha.support.DefaultGameDefinitionFileCreator /tmp/default-gamedefinition.json
+
+cat gamedefinition.json
