1005b39fa5dd88c6a0d14be7b7532b75d06be3ca
Hamilton Lima <hamilton.lima@gmail.com>
2018-12-29 03:03:53 -0500

    adds script to generate client API

--- generate-api-client.sh
+++ generate-api-client.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 wget http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.3.1/swagger-codegen-cli-2.3.1.jar -O swagger-codegen-cli.jar
 java -jar swagger-codegen-cli.jar generate --output ./sdk --lang java --input-spec ../robolucha-api/docs/swagger/swagger.yaml
-mv -r sdk/src/* ./src
-rm swagger-codegen-cli.jar
 
+rm sdk/src/main/AndroidManifest.xml
+cp -R sdk/src .
+rm -rf sdk
+rm swagger-codegen-cli.jar
