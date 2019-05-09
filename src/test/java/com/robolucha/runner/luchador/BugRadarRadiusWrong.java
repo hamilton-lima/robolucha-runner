a3912ab4aa67b0f753fc348bc7b6a6c5246c435e
Hamilton Lima <hamilton.lima@gmail.com>
2019-05-09 18:21:16 -0400

    Fixes assertion

--- src/test/java/com/robolucha/runner/luchador/BugRadarRadiusWrong.java
+++ src/test/java/com/robolucha/runner/luchador/BugRadarRadiusWrong.java
@@ -10,29 +10,20 @@ import org.junit.Test;
 import com.robolucha.event.match.MatchEventVO;
 import com.robolucha.game.event.LuchadorEvent;
 import com.robolucha.game.event.LuchadorEventListener;
-import com.robolucha.game.event.MatchEventListener;
 import com.robolucha.game.event.OnFoundEvent;
 import com.robolucha.game.vo.LuchadorPublicStateVO;
-import com.robolucha.game.vo.MatchRunStateVO;
 import com.robolucha.models.Luchador;
-import com.robolucha.models.LuchadorMatchState;
 import com.robolucha.models.LuchadorPublicState;
-import com.robolucha.publisher.MessageEnvelope;
-import com.robolucha.publisher.RemoteQueue;
 import com.robolucha.runner.MatchRunner;
 import com.robolucha.test.MockLuchador;
 import com.robolucha.test.MockMatchRunner;
 
-import io.reactivex.Observable;
 import io.reactivex.functions.Consumer;
-import io.reactivex.subjects.BehaviorSubject;
-
-
 
 public class BugRadarRadiusWrong {
-	
+
 	private static Logger logger = Logger.getLogger(BugRadarRadiusWrong.class);
-	
+
 	LuchadorPublicState finalState = null;
 	protected int defaultLife;
 	protected int bodyCount;
@@ -41,47 +32,15 @@ public class BugRadarRadiusWrong {
 	public int notFoundCounter;
 	int foundCount = 0;
 
-	private class RemoteQueueInspector extends RemoteQueue {
-		
-		public RemoteQueueInspector() {
-		}
-
-		@Override
-		public Observable<Long> publish(String channel, Object subjectToPublish) {
-			lastPublished = subjectToPublish;
-			MatchRunStateVO publishedState = (MatchRunStateVO) ((MessageEnvelope) lastPublished).message; 
-			logger.debug(">> Publishing data: luchadores.size=" + publishedState.luchadores.size());
-
-			found = null;
-			
-			publishedState.luchadores.forEach((luchador) -> {
-				if (luchador.state.id == 2L) {
-					found = luchador;
-				}
-			});
-
-			if (found == null) {
-				notFoundCounter++;
-			}
-
-			return Observable.just(0L);
-		}
-
-		public <T> BehaviorSubject subscribe(String channel, Class<T> clazzToSubscribe) {
-			return BehaviorSubject.create();
-		}
-	}
-
-	
 	@Before
 	public void setUp() throws Exception {
+		foundCount = 0;
 	}
 
 	@Test
 	public void testRun() throws Exception {
 		logger.setLevel(Level.DEBUG);
-		RemoteQueueInspector queue = new RemoteQueueInspector();
-		MatchRunner match = MockMatchRunner.build(5000, queue);
+		MatchRunner match = MockMatchRunner.build(1000);
 		match.getGameDefinition().setMinParticipants(2);
 		Luchador a = MockLuchador.build(1L, MethodNames.ON_REPEAT, "--turnGun(90)");
 		Luchador b = MockLuchador.build(-2L, MethodNames.ON_REPEAT, "--turnGun(90)");
@@ -104,8 +63,10 @@ public class BugRadarRadiusWrong {
 				runnerA.getState().setX(100);
 				runnerA.getState().setY(100);
 				
+				int closeEnough = (int) (runnerA.getState().getX() + runnerB.getGameComponent().getRadarRadius() - 1);
+
 				runnerB.getState().setGunAngle(180);
-				runnerB.getState().setX(299);
+				runnerB.getState().setX(closeEnough);
 				runnerB.getState().setY(100);
 
 				logger.debug(">> luchador A: " + runnerA.getState().getPublicState());
@@ -113,32 +74,27 @@ public class BugRadarRadiusWrong {
 			}
 
 		});
-		
-		match.addListener(new LuchadorEventListener(){
-			public void listen(LuchadorEvent event){
-				logger.info(">>> event : " + event) ;
-				if (event instanceof OnFoundEvent){
+
+		match.addListener(new LuchadorEventListener() {
+			public void listen(LuchadorEvent event) {
+				logger.info(">>> event : " + event);
+				if (event instanceof OnFoundEvent) {
 					logger.debug("FOUND OTHER! " + event);
 					foundCount++;
 				}
 			}
 		});
-		
 
-        MockMatchRunner.start(match);
+		MockMatchRunner.start(match);
 
 		match.getOnMatchEnd().blockingSubscribe(new Consumer<MatchEventVO>() {
 			public void accept(MatchEventVO arg0) throws Exception {
 				logger.debug("END! foundCount = " + foundCount);
-				LuchadorRunner runner = match.getRunners().get(-2L);
-				finalState = runner.getState().getPublicState();
+				assertTrue("onFound event was triggered " + foundCount, foundCount > 0);
 			}
 		});
-		
-        logger.info("START >>>");
-        
-        assertTrue("onFound event was triggered " + foundCount, foundCount > 0);
-        
+
+		logger.info("START >>>");
 	}
-	
+
 }
